e20e7b66de08063ce262ea749e9765b6b5cd2096
Christopher Ventura <chrisventura@outlook.com.br>
2022-01-27 17:08:09 -0300

    .

--- Aula_02/exercicios/exercicio4.js
+++ Aula_02/exercicios/exercicio4.js
@@ -1,6 +1,6 @@
 var prompt = require('prompt-sync')();
 
-let nota = ["NOTA", "9.0", "DEZ", "4.5", "7.0"];
+let nota = ["NOTAS", "9.0", "DEZ", "4.5", "7.0"];
 let nomes = ["ALUNO","ALINE", "MÁRIO", "SÉRGIO", "SHIRLEY"];
 
-console.log(`${nomes[0]} `);
+console.log(`${nomes}`,`\n${nota}`);
---
+++ Aula_09/exercicios/exercicio1.js
@@ -0,0 +1,17 @@
+var prompt = require('prompt-sync')();
+
+// 1 - Faça um programa que peça 2 números
+// 2 - e crie um array contando todos os números sequenciais do primeiro até o segundo, 
+// 3 - independente de ser maior ou menor.
+
+const num1 = 4;
+const num2 = 10;
+
+const numeros = [];
+
+for (let i = 0; i < num2.length; i++) {
+    console.log(numeros)    
+    while(num1 <= num2){
+        console.log(numeros)
+    }
+}
