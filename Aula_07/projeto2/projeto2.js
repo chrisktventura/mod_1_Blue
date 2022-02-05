c47db9e5fc9119d4b4dde8b0f30dd69b494249ec
Christopher Ventura <chrisventura@outlook.com.br>
2022-02-05 16:50:23 -0300

    projeto2 - JokenPô

--- Aula_07/projeto2/projeto2.js
+++ Aula_07/projeto2/projeto2.js
@@ -1,62 +1,69 @@
 var prompt = require("prompt-sync")();
 
 let rodadas = +prompt("Escolha quantas rodadas deseja jogar: ");
-let cont = 1;
+let cont = 0;
 let placarJ = 0;
 let placarC = 0;
 let desCont = 0;
+let escolhas = ["pedra","papel","tesoura"];
 
 while (true) {
   if (desCont === 1) {
     break;
   }
-  while (cont <= rodadas) {
+  for(i = 0; i < rodadas; i++) {
     console.log("rodadas --> ", rodadas);
-    console.log("#### JokenPo ####\n");
-    console.log("1 - Pedra \n2 - Papel \n3 - Tesoura\n");
+    console.log("#### JokenPô ####\n");
+    console.log("Pedra - Papel  - Tesoura\n");
 
     // lógica
-    let computador = Math.floor(Math.random() * 3 + 1);
+    const computador = Math.floor(Math.random() * 3 + 1);
 
     // lógica de escolha do jogador - 1
-    let jogador = prompt("Faça sua jogada: ");
+    let jogador = prompt("Faça sua jogada: ").trim().toLowerCase();
+    console.log();
     // validação de entrada 
-    while (jogador < 1 || jogador > 3) {
+    while (jogador != "pedra" && jogador != "papel" && jogador != "tesoura") {
       console.log("Opção Inválida");
-      jogador = +prompt("Escolha uma opção válida: ");
-    } // lógica de escolha jogador - 2
-    if (jogador == 1) {
-      console.log("Jogador escolheu Pedra!\n");
-    } else if (jogador == 2) {
-      console.log("Jogador escolheu Papel\n");
-    } else if (jogador == 3) {
-      console.log("Jogador escolheu Tesoura\n");
+      jogador = prompt("Escolha uma opção válida: ").trim().toLowerCase();
     }
+     // lógica de escolha jogador - 2
+    if (jogador === "pedra") {
+      console.log("Jogador escolheu: Pedra!\n");
+    } else if (jogador === "papel") {
+      console.log("Jogador escolheu: Papel\n");
+    } else if (jogador === "tesoura") {
+      console.log("Jogador escolheu: Tesoura\n");
+    }
+  
     // lógica de escolha computador - 1
     if (computador == 1) {
-      console.log("Computador escolheu Pedra!\n");
+      console.log("Computador escolheu: Pedra!\n");
     } else if (computador == 2) {
-      console.log("Computador escolheu Papel!\n");
+      console.log("Computador escolheu: Papel!\n");
     } else if (computador == 3) {
-      console.log("Computador escolheu Tesoura!\n");
+      console.log("Computador escolheu: Tesoura!\n");
     }
     // comparação de resultados
-    if (jogador == computador) {
-      console.log("Empatou");
+    if (jogador == "pedra" && computador == 1 || jogador == "papel" && computador == 2 || jogador == "tesoura" && computador == 3 ) {
+      cont++
+      console.log(`Empatou a rodada ${cont}`);
+      
     } else if (
-      (jogador == 1 && computador == 3) ||
-      (jogador == 2 && computador == 1) ||
-      (jogador == 3 && computador == 2)
+      (jogador == "pedra" && computador == 3) ||
+      (jogador == "papel" && computador == 1) ||
+      (jogador == "tesoura" && computador == 2)
     ) {
       placarJ++;
-      console.log(`Jogador vence a rodada ${cont}`);
+      cont++
+      console.log(`Jogador vence a rodada ${cont} !`);
     } else {
       placarC++;
-      console.log(`Computador vence a rodada ${cont} `);
+      cont++
+      console.log(`Computador vence a rodada ${cont} !`);
     }
-    console.log(`\nPlacar \nJogador  ${placarJ}  X  ${placarC}  Computador \n`);
+    console.log(`\n______ Placar _____\nJogador  ${placarJ}  X  ${placarC}  Computador \n`);
 
-    cont++;
   } // placar
   if (placarC > placarJ) {
     console.log(
@@ -67,11 +74,11 @@ while (true) {
       `O Jogador é o CAMPEÃO com ${placarJ} pontos em ${rodadas} rodadas!\n`
     );
   } else {
-    console.log("O jogo terminou EMPATADO ");
+    console.log("O jogo terminou EMPATADO!!\n");
   }
   desCont = +prompt("Deseja encerrar ? Se sim digite 1: ");
   if (desCont != 1) {
-    cont = 1;
+    cont = 0;
     placarC = 0;
     placarJ = 0;
     rodadas = +prompt("Escolha quantas rodadas deseja jogar: ");
